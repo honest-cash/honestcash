3d905dd9392db1dc750a4fe2390d4e5739deeebd
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-11-29 02:17:39 +0100

    broken webpack WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+const Uglify = require("uglifyjs-webpack-plugin");
+const BabelEnginePlugin = require('babel-engine-plugin');
+const UglifyEsPlugin = require('uglify-es-webpack-plugin');
+
 module.exports = {
     entry: './src/hashtagApp.js',
-    mode: 'development',
+    mode: 'production',
     output: { 
         path: __dirname + "/public/js",
         filename: 'bundle.js'
@@ -20,6 +24,33 @@ module.exports = {
             }
         ]
     },
+   
+    plugins: [
+        new Uglify({
+            uglifyOptions: {
+                cache: true,
+                ecma: 7,
+                compress: {
+                    warnings: false,
+                    // Disabled because of an issue with Uglify breaking seemingly valid code:
+                    // https://github.com/facebookincubator/create-react-app/issues/2376
+                    // Pending further investigation:
+                    // https://github.com/mishoo/UglifyJS2/issues/2011
+                    comparisons: false,
+                },
+                mangle: {
+                    keep_fnames: true,
+                    reserved: ["BigInteger","ECPair","Point"],
+                },
+                output: {
+                    //comments: false,
+                    // Turned on because emoji and regex is not minified properly using default
+                    // https://github.com/facebookincubator/create-react-app/issues/2488
+                    //ascii_only: true,
+                },
+            },
+        }),
+    ],
     resolve: {
         extensions: [ '.tsx', '.ts', '.js' ]
     }
