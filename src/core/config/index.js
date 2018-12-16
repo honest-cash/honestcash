551b9d2f7721aeb4bbdbe987b641023ca9ffc42c
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-12-16 02:57:21 +0100

    edit

--- src/core/config/index.js
+++ src/core/config/index.js
@@ -2,6 +2,6 @@ const PRODUCTION = "https://honest.cash/api";
 const STAGE = "https://stage.honest.cash/api";
 const DEV = "http://localhost:8080/api";
 
-let api = DEV;
+let api = PRODUCTION;
 
 angular.module("vqConfig", []).constant("API_URL", api);
