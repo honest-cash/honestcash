1a5ab9688396b3b298c134eba7acdbeef5d34f1b
Adrian Barwicki <adrianbarwicki@gmail.com>
2019-01-13 09:40:04 +0100

    fixed sharing

--- src/core/config/index.js
+++ src/core/config/index.js
@@ -2,8 +2,9 @@ const PRODUCTION = "https://honest.cash";
 const STAGE = "https://stage.honest.cash";
 const DEV_API = "http://localhost:8080";
 const DEV_CLIENT = "http://localhost:3010";
+const PRODUCTION_CLIENT = "https://honest.cash";
 
 export let api = `${PRODUCTION}/api`;
-export const client = DEV_CLIENT
+export const client = PRODUCTION_CLIENT
 
 angular.module("vqConfig", []).constant("API_URL", api);
