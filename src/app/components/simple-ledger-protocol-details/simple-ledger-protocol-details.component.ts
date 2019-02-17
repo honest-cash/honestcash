940023140ce04c147a907552c6d8d484b673b6ec
Adrian Barwicki <adrianbarwicki@gmail.com>
2019-02-17 17:36:33 +0100

    refactor

--- src/app/components/simple-ledger-protocol-details/simple-ledger-protocol-details.component.ts
+++ src/app/components/simple-ledger-protocol-details/simple-ledger-protocol-details.component.ts
@@ -1,11 +1,11 @@
-import './simple-ledger-protocol-details.styles.less';
-import template from './simple-ledger-protocol-details.template.html';
+import "./simple-ledger-protocol-details.styles.less";
+import template from "./simple-ledger-protocol-details.template.html";
 
 import tippy from "tippy.js";
-import 'tippy.js/dist/tippy.css';
-import { User } from '../../../core/models/models';
-import BitcoinComService from '../../../core/services/BitcoinComService';
-import ScopeService from '../../../core/services/ScopeService';
+import "tippy.js/dist/tippy.css";
+import { User } from "../../../core/models/models";
+import BitcoinComService from "../../../core/services/BitcoinComService";
+import ScopeService from "../../../core/services/ScopeService";
 
 interface ISimpleLedgerProtocolDetailsScope extends ng.IScope{
   address: string;
@@ -15,9 +15,9 @@ interface ISimpleLedgerProtocolDetailsScope extends ng.IScope{
 
 class SimpleLedgerProtocolDetailsController {
   public static $inject = [
-    '$scope',
-    'BitcoinComService',
-    'ScopeService'
+    "$scope",
+    "BitcoinComService",
+    "ScopeService"
   ];
 
   public addressBCH: string;
@@ -33,43 +33,49 @@ class SimpleLedgerProtocolDetailsController {
     this.ngOnInit();
   }
 
-  private ngOnInit() {
-    this.addressBCH = this.$scope.profile.addressBCH;
+  private async ngOnInit() {
+    this.addressSLP = this.$scope.profile.addressSLP;
 
-    if (this.addressBCH) {
-      this.bitcoinComService.convertBCHAddressToSLPAddress(this.addressBCH)
-      .then(conversionResult => {
-        this.addressSLP = conversionResult.slpAddress;
-        this.bitcoinComService.getSLPAddressBalance(this.addressSLP)
-        .then(balanceResult => {
-          if (balanceResult.length) {
-            const firstTransaction = balanceResult[0];
-            const balance: number = parseInt(firstTransaction.balance);
-            if (balance > 0) {
-              this.balanceSLP = `${Math.round(balance / 1000)}K`;
-              this.isVisible = true;
-
-              this.scopeService.safeApply(this.$scope);
-
-              tippy(".hc-tokens-badge", {
-                content: `Proud owner of ${this.balanceSLP} Honest Tokens`
-              });
-            }
-          }
-
-        });
-      });
+    if (!this.addressSLP) {
+      return;
     }
+
+    const balanceResult = await this.bitcoinComService.getSLPAddressBalance(this.addressSLP);
+
+    const token = balanceResult.find(
+      balance => balance.tokenId ==="c35a87afad11c8d086c1449ffd8b0a84324e72b15b1bcfdf166a493551b4eea6"
+    );
+
+    if (!token) {
+      return;
+    }
+
+    // tslint:disable-next-line: radix
+    const balance = parseInt(token.balance);
+
+    if (balance <= 0) {
+      return;
+    }
+
+    this.isVisible = true;
+
+    this.balanceSLP = `${Math.round(balance / 1000)}K`;
+
+    this.scopeService.safeApply(this.$scope);
+
+    tippy(".hc-tokens-badge", {
+      content: `Proud owner of ${this.balanceSLP} Honest Tokens`
+    });
   }
 }
 
 export default function simpleLedgerProtocolDetails(): ng.IDirective {
   return {
     controller: SimpleLedgerProtocolDetailsController,
-    controllerAs: 'simpleLedgerProtocolDetailsCtrl',
-    restrict: 'E',
+    controllerAs: "simpleLedgerProtocolDetailsCtrl",
+    restrict: "E",
     scope: {
-      profile: '=',
+      profile: "="
     },
     replace: true,
     template
