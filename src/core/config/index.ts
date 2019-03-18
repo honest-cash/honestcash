1429be121709f3d1119b19f6d543db41e3753a69
Adrian Barwicki <adrian@vq-labs.com>
2019-03-18 21:04:08 +0100

    refactor(config)

--- src/core/config/index.ts
+++ src/core/config/index.ts
@@ -1,13 +1,8 @@
 const PRODUCTION = "https://honest.cash";
 const DEV = "http://localhost:8080";
 
-const DEV_CLIENT = "http://localhost:3010";
-
-const PRODUCTION_CLIENT = "https://honest.cash";
-
-export const api = `${DEV}/api`;
-export const client = PRODUCTION_CLIENT;
+export const api = `${PRODUCTION}/api`;
+export const client = PRODUCTION;
+export const dateFormat = "MMM Do YY";
 
 angular.module("vqConfig", []).constant("API_URL", api);
-
-export const dateFormat = "MMM Do YY";
