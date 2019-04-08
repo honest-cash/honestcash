f88a7f741b980c5a353e3bb524f21b748e0f855c
Sercan AKMAN <sercanakman@gmail.com>
2019-04-08 15:03:19 +0300

    removed unused injection on UserProps

--- src/core/services/UserPropsService.ts
+++ src/core/services/UserPropsService.ts
@@ -3,14 +3,10 @@ import { IGlobalScope } from "../lib/interfaces";
 export default class UserPropsService {
   public static $inject = [
     "$rootScope",
-    "$http",
-    "API_URL",
   ];
 
   constructor(
     private $rootScope: IGlobalScope,
-    private $http: ng.IHttpService,
-    private API_URL,
   ) {}
 
   public checkRecoveryBackup = () => {
