cc6a96448c167089502336f3fb45adcb67746d4a
Sercan AKMAN <sercanakman@gmail.com>
2019-04-08 14:54:23 +0300

    BitcoinComService fixed

--- src/core/services/BitcoinComService.ts
+++ src/core/services/BitcoinComService.ts
@@ -10,29 +10,33 @@ interface IBitcoinComBalanceRequestResult {
   decimalCount: number;
 }
 
-
 export default class BitcoinComService {
 
   public static $inject = [
-    "$http"
+    "$http",
   ];
 
   constructor(
-    private $http: ng.IHttpService
+    private $http: ng.IHttpService,
   ) {
     this.ngOnInit();
   }
 
   private ngOnInit() {}
 
-  public async getSLPAddressBalance(slpAddress: string): Promise<IBitcoinComBalanceRequestResult[]> {
-    const res = await this.$http.get(`https://rest.bitcoin.com/v2/slp/balancesForAddress/${slpAddress}`);
+  public async getSLPAddressBalance(slpAddress: string):
+  Promise<IBitcoinComBalanceRequestResult[]> {
+    const res = await this.$http.get(
+      `https://rest.bitcoin.com/v2/slp/balancesForAddress/${slpAddress}`,
+    );
     return res.data as IBitcoinComBalanceRequestResult[];
   }
 
-
-  public async convertBCHAddressToSLPAddress(bchAddress: string): Promise<IBitcoinComAddressRequestResult> {
-    const res = await this.$http.get(`https://rest.bitcoin.com/v2/slp/address/convert/${bchAddress}`);
+  public async convertBCHAddressToSLPAddress(bchAddress: string):
+  Promise<IBitcoinComAddressRequestResult> {
+    const res = await this.$http.get(
+      `https://rest.bitcoin.com/v2/slp/address/convert/${bchAddress}`,
+    );
     return res.data as IBitcoinComAddressRequestResult;
 
   }
