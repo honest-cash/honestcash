8fc76c71487ae66b1cc53cfe97a365d051167489
Sercan AKMAN <sercanakman@gmail.com>
2019-04-08 15:01:04 +0300

    PostService fixed

--- src/core/services/PostService.ts
+++ src/core/services/PostService.ts
@@ -3,49 +3,49 @@ import { dateFormat } from "../../core/config/index";
 import SocialSharing from "../lib/SocialSharing";
 import { IFetchPostsArgs, Post, Upvote, Unlock } from "../models/models";
 
-var sanitizeHtml = require("sanitize-html");
+const sanitizeHtml = require("sanitize-html");
 
 export default class PostService {
   public static $inject = [
     "$http",
     "$sce",
-    "API_URL"
+    "API_URL",
   ];
 
   constructor(
     private $http: ng.IHttpService,
     private $sce,
-    private API_URL
+    private API_URL,
   ) {}
 
   public publishPic(postId, params, callback) {
-    this.$http.put(this.API_URL + "/post/image/publish", {
+    this.$http.put(`${this.API_URL}/post/image/publish`, {
       postId,
-      tags: params.hashtags
+      tags: params.hashtags,
     }).then((response) => {
       callback(response.data);
     });
   }
 
   public upvote(upvote) {
-    return this.$http.post(this.API_URL + "/post/" + upvote.postId + "/upvote", {
+    return this.$http.post(`${this.API_URL}/post/${upvote.postId}/upvote`, {
       postId: upvote.postId,
-      txId: upvote.txId
+      txId: upvote.txId,
     });
   }
 
   public unlock(unlock) {
-    return this.$http.post(this.API_URL + "/post/" + unlock.postId + "/unlock", {
+    return this.$http.post(`${this.API_URL}/post/${unlock.postId}/unlock`, {
       postId: unlock.postId,
-      txId: unlock.txId
+      txId: unlock.txId,
     });
   }
 
   public createRef(ref) {
-      return this.$http.post(this.API_URL + "/post/" + ref.postId + "/ref", {
-          extId: ref.extId,
-          postId: ref.postId
-      });
+    return this.$http.post(`${this.API_URL}/post/${ref.postId}/ref`, {
+      extId: ref.extId,
+      postId: ref.postId,
+    });
   }
 
   public async createPost(post: Post): Promise<Post> {
@@ -55,43 +55,43 @@ export default class PostService {
   }
 
   public async archivePost(post: Post) {
-    return this.$http.put(this.API_URL + "/post/" + post.id + "/archive", post);
+    return this.$http.put(`${this.API_URL}/post/${post.id}/archive`, post);
   }
 
   public displayHTML(html: string): string {
     const clean = sanitizeHtml(html, {
-      allowedTags: [ "h1", "h2", "h3", "h4", "h5", "h6", "blockquote", "p", "a", "ul", "ol",
-        "nl", "li", "b",  "strong", "img" ,"em", "strike", "code", "hr", "br", "pre", "iframe" ],
+      allowedTags: ["h1", "h2", "h3", "h4", "h5", "h6", "blockquote", "p", "a", "ul", "ol",
+        "nl", "li", "b",  "strong", "img" , "em", "strike", "code", "hr", "br", "pre", "iframe"],
       allowedAttributes: {
-        a: [ "href", "name", "target" ],
+        a: ["href", "name", "target"],
         // We don"t currently allow img itself by default, but this
         // would make sense if we did. You could add srcset here,
         // and if you do the URL is checked for safety
-        img: [ "src" ],
-        p: [ "class" ],
+        img: ["src"],
+        p: ["class"],
 
-        iframe: [ "src", "allowfullscreen" ],
+        iframe: ["src", "allowfullscreen"],
       },
       // Lots of these won"t come up by default because we don"t allow them
-      selfClosing: [ "img", "br", "hr", "area", "base", "basefont", "input", "link", "meta" ],
+      selfClosing: ["img", "br", "hr", "area", "base", "basefont", "input", "link", "meta"],
       // URL schemes we permit
-      allowedSchemes: [ "http", "https" ],
+      allowedSchemes: ["http", "https"],
       allowedSchemesByTag: {},
-      allowedSchemesAppliedToAttributes: [ "href", "src", "cite" ],
-      allowProtocolRelative: true
+      allowedSchemesAppliedToAttributes: ["href", "src", "cite"],
+      allowProtocolRelative: true,
     });
 
     return this.$sce.trustAsHtml(clean);
   }
 
   public async getById(postId: number): Promise<Post> {
-    const res = await this.$http.get(this.API_URL + "/post/" + postId);
+    const res = await this.$http.get(`${this.API_URL}/post/${postId}`);
 
     return this.processPost(res.data as Post);
   }
 
   public async getByAlias(username, alias): Promise<Post> {
-    const res = await this.$http.get(this.API_URL + "/post/" + username + "/" + alias);
+    const res = await this.$http.get(`${this.API_URL}/post/${username}/${alias}`);
 
     return this.processPost(res.data as Post);
   }
@@ -103,19 +103,19 @@ export default class PostService {
   }
 
   public async getResponses(postId: number): Promise<Post[]> {
-    const res = await this.$http.get(this.API_URL + "/post/" + postId + "/responses");
+    const res = await this.$http.get(`${this.API_URL}/post/${postId}/responses`);
 
     return (res.data as Post[]).map((post) => this.processPost(post));
   }
 
   public async getUnlocks(postId: number): Promise<Unlock[]> {
-    const res = await this.$http.get(this.API_URL + "/post/" + postId + "/unlocks");
+    const res = await this.$http.get(`${this.API_URL}/post/${postId}/unlocks`);
 
     return res.data as Unlock[];
-  }  
+  }
 
   public getUserUnlocks(userId?: number, callback?): void {
-    this.$http.get(`${this.API_URL}/posts/unlocks${userId ? "?userId=" + userId : ""}`)
+    this.$http.get(`${this.API_URL}/posts/unlocks${userId ? `?userId=${userId}` : ""}`)
     .then((response) => {
       const unlocks = (response.data as Unlock[]).map(this.processUnlock);
       callback(unlocks);
@@ -126,7 +126,7 @@ export default class PostService {
     this.$http({
       method: "GET",
       params: query,
-      url: this.API_URL + "/posts"
+      url: `${this.API_URL}/posts`,
     }).then((response) => {
       const feeds = (response.data as Post[]).map(this.processPost);
 
@@ -161,7 +161,7 @@ export default class PostService {
       unlock.userPost = this.processPost(unlock.userPost);
       (unlock.userPost as any).unlockedAtFormatted = this.formatDate(unlock.createdAt);
     }
-    
+
     return unlock;
   }
 }
