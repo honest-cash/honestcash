a77bbbe68406a20ab17d42a354ea996bfcfb0d60
Sercan AKMAN <sercanakman@gmail.com>
2019-04-08 14:41:15 +0300

    uncensorable-button.component fixed editorctrl had interfaces of dependencies, they are not exported and used in uncensorable button

--- typings.d.ts
+++ typings.d.ts
@@ -1,10 +1,11 @@
-declare module 'angular';
-declare module 'async';
-declare module 'medium-editor';
-declare module 'showdown';
-declare module 'tippy.js';
-declare module 'qrcode';
-declare module 'ng-infinite-scroll';
+declare module "angular";
+declare module "async";
+declare module "medium-editor";
+declare module "showdown";
+declare module "tippy.js";
+declare module "qrcode";
+declare module "ng-infinite-scroll";
+declare module "lzutf8";
 
-declare module '*.less';
-declare module '*.html';
+declare module "*.less";
+declare module "*.html";
