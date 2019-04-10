a4619fa620cbe6cf6ea1ffb9ef4bccdd00dc2c41
Adrian Barwicki <adrian@vq-labs.com>
2019-04-10 21:22:16 +0200

    added welcome module shell for v2

---
+++ honestcash-v2/src/app/modules/welcome/pages/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
