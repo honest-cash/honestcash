e87f64f036009f18eed4578b0601e57f3b5f7b3c
Adrian Barwicki <adrian@vq-labs.com>
2019-04-17 01:42:29 +0200

    refactor(editor): messy code cleanup

---
+++ spec/EditorService.spec.ts
@@ -0,0 +1,15 @@
+import EditorService from "../src/editor/services/EditorService";
+// import * as jquery from "jquery";
+
+// const editorService = new EditorService(jquery);
+
+// @todo implemnt this
+describe("editorService", () => {
+  it("dummy test", () => {
+    /**
+    const fixedBody = editorService.getFixedBody("<div>Hello world</div>");
+
+    console.log(fixedBody);
+     */
+  });
+});
