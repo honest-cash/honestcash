5624bd3634d10e64eb5d97e0ea4e62c1eb65ed71
Sercan AKMAN <sercanakman@gmail.com>
2019-04-29 12:08:05 +0300

    loading indication on buttons in v2 welcome module (#120)
    * added fat loading button to displays "Please Wait..." while the button has loading state
    
    * added fat loading button to welcome module
    
    * added fat loading button to welcome pages
    
    * added fontawesome from cdn,
    added loadingText to be specified in fat-loading-button instead of Please Wait... as suggested by Jenny,
    also added a spinner (font-awesome)

---
+++ honestcash-v2/src/app/modules/welcome/components/fat-loading-button/fat-loading-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-fat-loading-button',
+  templateUrl: './fat-loading-button.component.html',
+  styleUrls: ['./fat-loading-button.component.scss']
+})
+export class FatLoadingButtonComponent implements OnInit {
+
+  @Input() isLoading;
+  @Input() loadingText;
+  @Input() text;
+
+  constructor(
+
+  ) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
