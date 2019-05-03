b456abb9e5d73c7381917e872cf9889f21f9155b
Sercan AKMAN <sercanakman@gmail.com>
2019-05-03 03:10:29 +0300

    completed tests for token interceptor

--- honestcash-v2/src/app/core/http/token.interceptor.spec.ts
+++ honestcash-v2/src/app/core/http/token.interceptor.spec.ts
@@ -2,48 +2,56 @@ import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 
-import { environment } from '../../../environments/environment';
-import { ApiPrefixInterceptor } from './api-prefix.interceptor';
+import { TokenInterceptor } from './token.interceptor';
+import {MockAuthenticationService} from '..';
+import {AuthenticationService} from '../services/authentication.service';
 
-describe('ApiPrefixInterceptor', () => {
+describe('TokenInterceptor', () => {
   let http: HttpClient;
   let httpMock: HttpTestingController;
+  let authenticationService: MockAuthenticationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
+        { provide: AuthenticationService, useClass: MockAuthenticationService },
         {
           provide: HTTP_INTERCEPTORS,
-          useClass: ApiPrefixInterceptor,
+          useClass: TokenInterceptor,
           multi: true
         }
       ]
     });
   });
 
-  beforeEach(inject([HttpClient, HttpTestingController], (_http: HttpClient, _httpMock: HttpTestingController) => {
+  beforeEach(inject([HttpClient, HttpTestingController, AuthenticationService], (_http: HttpClient, _httpMock: HttpTestingController, _authenticationService: MockAuthenticationService) => {
     http = _http;
     httpMock = _httpMock;
+    authenticationService = _authenticationService;
   }));
 
   afterEach(() => {
     httpMock.verify();
   });
 
-  it('should prepend environment.apiUrl to the request url', () => {
+  it('should append token to the request headers as x-auth-token if the user is authenticated', () => {
+    authenticationService.isAuthenticated = true;
     // Act
-    http.get('/toto').subscribe();
+    http.get('http://test.com/toto').subscribe();
 
     // Assert
-    httpMock.expectOne({ url: environment.apiUrl + '/toto' });
+    const httpRequest = httpMock.expectOne({ url: 'http://test.com/toto' });
+    expect(httpRequest.request.headers.has('x-auth-token')).toEqual(true);
   });
 
-  it('should not prepend environment.apiUrl to request url', () => {
+  it('should not append token to the request headers as x-auth-token if the user is not authenticated', () => {
+    authenticationService.isAuthenticated = false;
     // Act
-    http.get('hTtPs://domain.com/toto').subscribe();
+    http.get('http://test.com/toto').subscribe();
 
     // Assert
-    httpMock.expectOne({ url: 'hTtPs://domain.com/toto' });
+    const httpRequest = httpMock.expectOne({ url: 'http://test.com/toto' });
+    expect(httpRequest.request.headers.has('x-auth-token')).toEqual(false);
   });
 });
