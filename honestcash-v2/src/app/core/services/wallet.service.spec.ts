505e2b2eed6a1d2646bbd43e47a8807787a26bca
Sercan AKMAN <sercanakman@gmail.com>
2019-05-06 19:39:39 +0300

    feat(tests): added test for WalletService

---
+++ honestcash-v2/src/app/core/services/wallet.service.spec.ts
@@ -0,0 +1,70 @@
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HttpService} from '..';
+import {mock} from '../helpers/mock';
+import {of} from 'rxjs';
+import {WALLET_LOCALSTORAGE_KEYS, WalletService} from './wallet.service';
+import wallet from "../models/wallet";
+
+const SHARED_MOCKS = {
+  mnemonic: 'test test2 test3 test4',
+};
+
+describe('WalletService', () => {
+  let walletService: WalletService;
+  let httpServiceMock: HttpService;
+
+  beforeEach(() => {
+    httpServiceMock = mock(HttpService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        WalletService,
+        {provide: HttpService, useValue: httpServiceMock}
+      ]
+    });
+    walletService = TestBed.get(WalletService);
+  });
+
+  afterEach(() => {
+    // Cleanup
+  });
+
+  describe('instance', () => {
+    it('should have been initialized', () => {
+      expect(walletService).toBeDefined();
+    });
+  });
+
+  describe('setWallet', () => {
+    it('should set the mnemonic in localStorage with correct key', () => {
+      // Act
+      walletService.setWallet(SHARED_MOCKS.mnemonic);
+      // Assert
+      expect(localStorage.getItem(WALLET_LOCALSTORAGE_KEYS.MNEMONIC)).toEqual(SHARED_MOCKS.mnemonic);
+    });
+  });
+
+  describe('unsetWallet', () => {
+    it('should unset the mnemonic in localStorage', () => {
+      // Act
+      localStorage.setItem(WALLET_LOCALSTORAGE_KEYS.MNEMONIC, SHARED_MOCKS.mnemonic);
+      walletService.unsetWallet();
+      // Assert
+      expect(localStorage.getItem(WALLET_LOCALSTORAGE_KEYS.MNEMONIC)).toBeNull();
+    });
+  });
+
+  describe('getWalletMnemonic', () => {
+    it('should get the mnemonic', () => {
+      // Act
+      walletService.setWallet(SHARED_MOCKS.mnemonic);
+      // Assert
+      expect(walletService.getWalletMnemonic()).toEqual(SHARED_MOCKS.mnemonic);
+    });
+  });
+
+});
--- honestcash-v2/src/app/core/services/wallet.service.ts
+++ honestcash-v2/src/app/core/services/wallet.service.ts
@@ -3,32 +3,33 @@ import SimpleBitcoinWallet from 'simple-bitcoin-wallet';
 import { WalletUtils } from '../../shared/lib/WalletUtils';
 import Wallet from '../models/wallet';
 
+export const WALLET_LOCALSTORAGE_KEYS = {
+  HD_PATH: 'HC_BCH_HD_PATH',
+  MNEMONIC: 'HC_BCH_MNEMONIC',
+  PRIVATE_KEY: 'HC_BCH_PRIVATE_KEY'
+};
+
+export const WALLET_DEFAULT_HD_PATH = `m/44'/0'/0'/0/0`;
+
 @Injectable({providedIn: 'root'})
 export class WalletService {
-  private WALLET_KEYS = {
-    HD_PATH: 'HC_BCH_HD_PATH',
-    MNEMONIC: 'HC_BCH_MNEMONIC',
-    PRIVATE_KEY: 'HC_BCH_PRIVATE_KEY'
-  };
-
-  private DEFAULT_HD_PATH = `m/44'/0'/0'/0/0`;
   _wallet: Wallet = null;
   wallet: Wallet = null;
 
   constructor() {}
 
   public getWalletMnemonic(): string {
-    return localStorage.getItem(this.WALLET_KEYS.MNEMONIC);
+    return localStorage.getItem(WALLET_LOCALSTORAGE_KEYS.MNEMONIC);
   }
 
   public setWallet(mnemonic: string): void {
-    localStorage.setItem(this.WALLET_KEYS.MNEMONIC, mnemonic);
+    localStorage.setItem(WALLET_LOCALSTORAGE_KEYS.MNEMONIC, mnemonic);
 
     return;
   }
 
   public unsetWallet(): void {
-    localStorage.removeItem(this.WALLET_KEYS.MNEMONIC);
+    localStorage.removeItem(WALLET_LOCALSTORAGE_KEYS.MNEMONIC);
 
     return;
   }
