5cfa11f6bd1a19dd7c37257bce0b681538ad8865
Sercan AKMAN <sercanakman@gmail.com>
2019-05-10 05:22:41 +0300

    feat(welcome): transitioned thank-you page to bootstrap

--- honestcash-v2/src/app/modules/welcome/pages/thank-you/thank-you.component.ts
+++ honestcash-v2/src/app/modules/welcome/pages/thank-you/thank-you.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppStates } from '../../../../app.states';
-import User from '../../../../core/models/user';
-
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-page-thank-you',
   templateUrl: './thank-you.component.html',
   styleUrls: ['./thank-you.component.scss']
 })
-export class ThankYouComponent implements OnInit {
-  @HostBinding('class') class = 'w-full flex flex-wrap content-center items-center justify-center';
-  @HostBinding('style.height') height = '75vh';
-  @HostBinding('style.minHeight') minHeight = '75vh';
-
-  isLoading = false;
-  user = new User();
-
-  constructor(
-    private store: Store<AppStates>
-  ) {}
-
-  ngOnInit() {}
-
+export class ThankYouComponent {
+  @HostBinding('class') class = 'card mb-auto mt-auto';
 }
