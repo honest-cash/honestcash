d42da1a80f7393d8436733d3114628bfd1426807
Sercan AKMAN <sercanakman@gmail.com>
2019-05-30 14:50:04 +0300

    fix(editor):
    fix of editor effects tests

--- honestcash-v2/src/app/store/editor/editor.effects.spec.ts
+++ honestcash-v2/src/app/store/editor/editor.effects.spec.ts
@@ -14,6 +14,7 @@ xdescribe('editor.effects', () => {
         HttpClientTestingModule,
       ],
       providers: [
+        EditorEffects,
         provideMockActions(() => actions),
       ],
     });
