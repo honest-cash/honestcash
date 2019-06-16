ab9dda6737cee7e8f0523252d340400a53064e3b
Adrian Barwicki <adrian@adrianbarwicki.com>
2019-06-16 14:03:46 +0200

    fix(editor): remove story preview for now

--- honestcash-v2/src/app/store/editor/editor.story.body.initial-value.ts
+++ honestcash-v2/src/app/store/editor/editor.story.body.initial-value.ts
@@ -1,44 +1,9 @@
 const blankBody = [
   {
-    'type': 'header',
-    'data': {
-      'text': '<b>Welcome to Honest Editor!</b>',
-      'level': 2
-    }
-  },
-  {
     'type': 'paragraph',
     'data': {
       // tslint:disable-next-line:max-line-length
-      'text': 'You can start by adding blocks by clicking in the empty area after the last paragraph in the story then by clicking the plus icon that appears on the left.'
-    }
-  },
-  {
-    'type': 'paragraph',
-    'data': {
-      // tslint:disable-next-line:max-line-length
-      'text': 'You can also format your text by selecting the text you want to make&nbsp;<b>bold, </b><i>italic, </i><mark class=\'cdx-marker\'>marker</mark><i> or </i><a href=\'https://honest.cash\'>a link</a>.'
-    }
-  },
-  {
-    'type': 'paragraph',
-    'data': {
-      // tslint:disable-next-line:max-line-length
-      'text': 'You can paste link URLs, image URLs, Youtube URLs for embedding or even raw copied images. Captions will become available when you paste the URL.'
-    }
-  },
-  {
-    'type': 'paragraph',
-    'data': {
-      // tslint:disable-next-line:max-line-length
-      'text': 'You can delete the blocks you want by clicking inside of them then clicking the ellipsis on top right corner of the block. The icons will guide you through the process. Remember to hit twice to confirm your deletion!'
-    }
-  },
-  {
-    'type': 'paragraph',
-    'data': {
-      // tslint:disable-next-line:max-line-length
-      'text': 'You might have already noticed that there are some up and down arrows in the block toolbar. You can move your sections up and down using those arrows. Unfortunately there is <b>no drag &amp; drop support</b> yet, please stay tuned!'
+      'text': 'Start writing here...'
     }
   }
 ];
