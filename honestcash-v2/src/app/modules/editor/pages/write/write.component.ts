3aae20fdf784c7e562bbae4811f69bd2ea34c90e
Sercan AKMAN <sercanakman@gmail.com>
2019-06-21 11:45:09 +0300

    fix(editor): disabled autosave until it is stable

--- honestcash-v2/src/app/modules/editor/pages/write/write.component.ts
+++ honestcash-v2/src/app/modules/editor/pages/write/write.component.ts
@@ -1,10 +1,6 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
-import {AppStates, selectEditorState} from '../../../../app.states';
-import {State as EditorState} from '../../../../store/editor/editor.state';
-import {Observable, Subscription} from 'rxjs';
-import {EDITOR_AUTO_SAVE} from '../../components/editor/editor.component';
-import Post from '../../../../shared/models/post';
+import {AppStates} from '../../../../app.states';
 import {EditorDraftLoad} from '../../../../store/editor/editor.actions';
 import {EDITOR_EDITING_MODES} from '../../components/header/header.component';
 
@@ -21,10 +17,10 @@ export class EditorWriteComponent implements OnInit {
   ) {
     // explicitly turn autosave on write mode
     // so that on edit mode it is default by default even if forgotten
-    EDITOR_AUTO_SAVE.ON = true;
+    // EDITOR_AUTO_SAVE.ON = true;
   }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.store.dispatch(new EditorDraftLoad());
   }
 }
