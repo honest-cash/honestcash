58d612b5c1bb468ad29ba283a09835543f44207d
Sercan AKMAN <sercanakman@gmail.com>
2019-07-05 18:13:58 +0300

    fix(editor): fix of shared modules

--- honestcash-v2/src/app/editor/editor.module.ts
+++ honestcash-v2/src/app/editor/editor.module.ts
@@ -4,16 +4,11 @@ import {FormsModule} from '@angular/forms';
 import {TagInputModule} from 'ngx-chips';
 import {EditorWriteComponent} from './pages/write/write.component';
 import {EditorEditComponent} from './pages/edit/edit.component';
-import {EditorPublishModalComponent} from './components/publish-modal/publish-modal.component';
 import {EditorPublishButtonComponent} from './components/publish-button/publish-button.component';
 import {EditorHeaderComponent} from './components/header/header.component';
-import {EditorPaidSectionSelectionComponent} from './components/paid-section-selection/paid-section-selection.component';
 import {EditorSaveStatusComponent} from './components/save-status/save-status.component';
-import {EditorComponent} from './components/editor/editor.component';
 import {EditorCommentButtonComponent} from './components/comment-button/comment-button.component';
 import {ScriptLoaderModule} from 'ngx-script-loader';
-import {EditorStoryTagsSelectionComponent} from './components/story-tags-selection/story-tags-selection.component';
-import {EditorPaidSectionToggleButtonComponent} from './components/paid-section-toggle-button/paid-section-toggle-button.component';
 import {EditorCommentComponent} from './pages/comment/comment.component';
 import {EditorParentPostBodyComponent} from './components/parent-post-body/parent-post-body.component';
 import {EditorRoutingModule} from './editor-routing.module';
@@ -24,10 +19,7 @@ import {UserSharedModule} from '../user/user-shared.module';
 @NgModule({
   declarations: [
     EditorHeaderComponent,
-    EditorPublishModalComponent,
-    EditorPublishButtonComponent,
     EditorCommentButtonComponent,
-    EditorSaveStatusComponent,
     EditorContainerComponent,
     EditorWriteComponent,
     EditorEditComponent,
