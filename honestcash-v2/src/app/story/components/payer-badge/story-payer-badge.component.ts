9f7ade293901060f84086b9d9270f3b7a0519a98
Sercan AKMAN <sercanakman@gmail.com>
2019-07-07 22:33:56 +0300

    fix(story): added profile links to upvote and unlock badges

--- honestcash-v2/src/app/story/components/payer-badge/story-payer-badge.component.ts
+++ honestcash-v2/src/app/story/components/payer-badge/story-payer-badge.component.ts
@@ -1,6 +1,7 @@
-import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, Inject, Input, OnInit} from '@angular/core';
 import {Upvote} from '../../models/upvote';
 import {Unlock} from '../../models/unlock';
+import {WindowToken} from '../../../../core/shared/helpers/window.helper';
 
 @Component({
   selector: 'story-payer-badge',
@@ -10,4 +11,15 @@ import {Unlock} from '../../models/unlock';
 export class StoryPayerBadgeComponent {
   @HostBinding('class') public class = 'col-1 mb-2 p-2';
   @Input() public transaction: Upvote | Unlock;
+
+  constructor(
+    @Inject(WindowToken) private window,
+  ) {
+  }
+
+  public goToUserProfile() {
+    if (this.transaction && this.transaction.user && this.transaction.user.username) {
+      this.window.location.href = `/profile/${this.transaction.user.username}`;
+    }
+  }
 }
